090f82c717a4d55413d1b2c18a67a3ec6c07651e
Aner Zakovitch <aner@gitatechnologies.com>
2019-05-22 08:44:20 -0700

    Added command, renumbering on TMUX

---
+++ scripts/az-bulbul.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "
+Straw-headed bulbul
+Striated bulbul
+Cream-striped bulbul
+Spot-necked bulbul
+Black-and-white bulbul
+Grey-headed bulbul
+Black-headed bulbul
+Andaman bulbul
+Black-capped bulbul
+Black-crested bulbul
+Ruby-throated bulbul
+Flame-throated bulbul
+Bornean bulbul
+Scaly-breasted bulbul
+Grey-bellied bulbul
+Red-whiskered bulbul
+Brown-breasted bulbul
+Light-vented bulbul
+Styan's bulbul
+Himalayan bulbul
+White-eared bulbul
+Red-vented bulbul
+Sooty-headed bulbul
+White-spectacled bulbul
+African red-eyed bulbul
+Cape bulbul
+Common bulbul
+Somali bulbul
+Dodson's bulbul
+Dark-capped bulbul
+Puff-backed bulbul
+Blue-wattled bulbul
+Yellow-wattled bulbul
+Orange-spotted bulbul
+Aceh bulbul
+Stripe-throated bulbul
+Yellow-throated bulbul
+Yellow-eared bulbul
+Flavescent bulbul
+Yellow-vented bulbul
+White-browed bulbul
+Olive-winged bulbul
+Ashy-fronted bulbul
+Ayeyarwady bulbul
+Streak-eared bulbul
+Cream-vented bulbul
+Asian red-eyed bulbul
+Spectacled bulbul
+Cream-eyed bulbul
+" | espeak
+
