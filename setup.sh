1bd842845b6ab3e18471e13f68e8b5c2c72d94be
Aner Zakovitch <anerisgreat@gmail.com>
2019-06-30 02:01:50 -0700

    Fixed up some stuff. The reveal.js still needs to be fixed

--- setup.sh
+++ setup.sh
@@ -7,15 +7,14 @@ check_if_exists () {
 check_if_exists_or_abort () {
     { ! check_if_exists $1 ; } && \
     echo "Must install $1 before proceeding! Aborting." && \
-    exit 1 || \
-    echo "$1 found"
+    exit 1 ;# || \
+    #echo "$1 found"
 }
 
 check_symlink() {
     tmp=$(find "$1" -maxdepth 1 -type l)
     target=$(readlink -f $tmp)
     [ $target == $2 ] && return 0 || return 1
-    return 1
 }
 
 
@@ -30,6 +29,24 @@ check_symlink_make_if_not() {
     echo "FEKKER FEKKER"
     ln -s $2 $1 && return 0
     return 1
+}
+
+sudo_check_symlink_make_if_not() {
+    check_symlink $1 $2 && return 0
+    find "$1" -maxdepth 1 -type l > /dev/null && sudo unlink $1 && \
+        sudo ln -s $3 $1 && return 0
+    find "$1" -maxdepth 1 -type f > /dev/null && sudo rm $1 && \
+        sudo ln -s $2 $1 && return 0
+    find "$1" -maxdepth 1 -type d > /dev/null && sudo rm -r $1 && \
+        sudo ln -s $2 $1 && return 0
+    sudo ln -s $2 $1 && return 0
+    return 1
+}
+
+make_folder_if_not_exists() {
+    { find "$1" -maxdepth 1 -type d > /dev/null || mkdir $1 -p ; } && return 0
+    echo "Could not find directory $1 and encountered error creating it!"
+    exit 1
 }
 
 [ "$USER" = root ] && echo "This script shouldn't be run as root. Aborting." \
@@ -93,7 +110,7 @@ check_if_exists libtoolize || {
     rm -rf $tmp libtool-2.4.6.tar.gz
 } || { echo 'Installation of libtoolize failed' ; exit 1 ; }
 
-command -v curl || { \
+check_if_exists curl || { \
     git clone https://github.com/curl/curl.git && \
     cd curl && \
     #env PKG_CONFIG_PATH=/usr/lib/pkgconfig ./configure --with-ssl
@@ -112,31 +129,42 @@ check_if_exists cmake || sudo snap install cmake --classic
 
 #NEOVIM
 {   check_if_exists nvim || \
-    { wget https://github.com/neovim/neovim/releases/download/v0.3.7/nvim.appimage && chmod u+x nvim.appimage && sudo mv ./nvim.appimage /usr/bin/nvim  ; } \
+    { wget https://github.com/neovim/neovim/releases/download/v0.3.7/nvim.appimage && \
+        chmod u+x nvim.appimage && sudo mv ./nvim.appimage /usr/bin/nvim  ; } \
 || { echo 'Installation of NVIM failed' ; exit 1; } } && { \
     #Post NVIM installation
-    echo "POST VIM"
-    nvim_location=$(command -v nvim) && \
-    sudo check_symlink_make_if_not '/usr/bin/vim' $nvim_location
+    {
+        nvim_location=$(command -v nvim) && \
+        sudo_check_symlink_make_if_not '/usr/bin/vim' $nvim_location
+    } || echo "nvim to vim symbolink linkage failed" ;
 }
 
 #sudo apt-get install -y inotify-tools
 
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+find "$HOME/.vim/bundle/Vundle.vim" -maxdepth 1 -type d || git clone \
+    https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-mkdir $HOME/.config/nvim
-ln -sf $PWD/conf_files/vim-config $HOME/.config/nvim/init.vim
-ln -sf $PWD/conf_files/vim-config $HOME/.vimrc
+#Mapping configurations
+make_folder_if_not_exists $HOME/.config/nvim
+check_symlink_make_if_not $HOME/.config/nvim/init.vim \
+    $PWD/conf_files/vim-config || \
+    { echo "nvim config link failed" && exit 1 ; }
+check_symlink_make_if_not $HOME/.vimrc $PWD/conf_files/vim-config || \
+    { echo "nvim config link failed" && exit 1 ; }
 
-ln -sf $PWD/conf_files/tmux-config $HOME/.tmux.conf
+check_symlink_make_if_not $HOME/.tmux.conf $PWD/conf_files/tmux-config || \
+    { echo "tmux config link failed" && exit 1 ; }
 
-mkdir $HOME/.config/i3
-rm $HOME/.config/i3/config
-ln -sf $PWD/conf_files/i3-config $HOME/.config/i3/config
+make_folder_if_not_exists $HOME/.config/i3
+check_symlink_make_if_not $HOME/.config/i3/config \
+    $PWD/conf_files/i3-config || \
+    { echo "i3 config link failed" && exit 1 ; }
 
-mkdir $HOME/.config/i3status
-ln -s $PWD/conf_files/i3status-config $HOME/.config/i3status/config
+make_folder_if_not_exists $HOME/.config/i3status
+check_symlink_make_if_not $HOME/.config/i3status/config \
+    $PWD/conf_files/i3status-config || \
+    { echo "i3 statusbar config link failed" && exit 1 ; }
 
 vim +PluginInstall +qall
 export TERM=xterm-256color
@@ -149,11 +177,15 @@ ls -la /usr/local/bin/ | grep "anerdevenv/scripts" |\
     while read line ; do sudo rm /usr/local/bin/$line ; done
 
 chmod -R a+x scripts
-sudo ln -s $PWD/scripts/* /usr/local/bin/
+sudo ln -s $PWD/scripts/* /usr/local/bin/ || \
+    { echo "Script linkage failed" && exit 1 ; }
 
 #fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf &&
-~/.fzf/install --all
+check_if_exists "fzf --version" || \
+{
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && \
+    sudo $HOME/.fzf/install --all ;
+} || { echo "fzf install failed" && exit 1 ; }
 
 #Installation of reveal js for pandoc
 #UNTIL THEY FIX IT
