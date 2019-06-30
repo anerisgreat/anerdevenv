eb56a449603572c7b9498f04543843f8c9dc685d
Aner Zakovitch <aner@gitatechnologies.com>
2019-06-30 09:45:46 -0700

    Got rid of stupid.

--- stupid.sh
+++
@@ -1,9 +0,0 @@
-check_symlink() {
-    tmp=$(find "$1" -maxdepth 1 -type l)
-    target=$(readlink -f $tmp)
-    [ $target == $2 ] && return 0 || return 1
-    return 1
-}
-
-{ check_symlink $HOME/.config/nvim/init.vim $PWD/conf_files/vim-config ; } && echo GOOD
-
