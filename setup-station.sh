9722e91a786022c3a7d9dffa9288612828a6de1e
Aner Zakovitch <anerisgreat@gmail.com>
2019-09-08 00:04:17 -0700

    Adjusted i3 terminal startup.

--- setup-station.sh
+++ setup-station.sh
@@ -5,6 +5,15 @@ source $PWD/helper-funcs/setup-helpers/setup-core.sh
 check_if_exists firefox || try_install_from_package_manager firefox || \
     { echo 'Installation of firefox failed' ; exit 1; }
 
+#st
+check_if_exists st || \
+{ \
+    cd submodules/st && \
+    make && sudo make install && \
+    cd ../../; \
+} || { echo 'Installation of st failed.' && exit 1; }
+
+
 #i3
 make_folder_if_not_exists $HOME/.config/i3
 check_symlink_make_if_not $HOME/.config/i3/config \
@@ -44,12 +53,5 @@ check_if_exists "fzf --version" || \
 check_symlink_make_if_not $HOME/.bashrc \
     $PWD/conf-files/bashrc || \
     { echo "bashrc link failed" && exit 1 ; }
-
-check_if_exists st || \
-{ \
-    cd submodules/st && \
-    make && sudo make install && \
-    cd ../../; \
-} || { echo 'Installation of st failed.' && exit 1; }
 
 install_scripts_from_folder scripts/station
