3343d20e197919e2132c1b9b344363c78d005481
Aner Zakovitch <anerisgreat@gmail.com>
2019-09-10 02:39:51 -0700

    Added installation of wget

--- setup-base.sh
+++ setup-base.sh
@@ -46,6 +46,10 @@ check_if_exists curl || try_install_from_package_manager curl || { \
 check_if_exists tmux || try_install_from_package_manager tmux || \
     { echo 'Installation of tmux failed' ; exit 1; }
 
+#tmux
+check_if_exists wget || try_install_from_package_manager wget || \
+    { echo 'Installation of wget failed' ; exit 1; }
+
 #xclip
 check_if_exists xclip || try_install_from_package_manager xclip || \
     { echo 'Installation of xclip failed' ; exit 1; }
